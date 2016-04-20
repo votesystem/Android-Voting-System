b6e56c20b34bb9037cd0df0cf468f9945060e41d
SonnyFernando <sonnyfernando95@gmail.com>
2016-04-20 10:47:41 +0700

    Added files via upload

---
+++ Revisi/welcome.java
@@ -0,0 +1,34 @@
+package com.votegubri;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class welcome extends Activity 
+{
+   
+   Button next;   
+   
+    /** Called when the activity is first created. */
+   
+   @Override
+   
+   
+    public void onCreate(Bundle savedInstanceState) 
+   {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+   }
+   
+        public void selanjutnya (View theButton)
+        {
+    	Intent i = new Intent(this,Menulayanan.class);
+    	startActivity(i);	
+        } 
+          
+   
+}
+
